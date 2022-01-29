b51a466fe4ad57fc1e341c1eb243ba010b5ba5ae
dialloflatno <brahimdiallo23@gmail.com>
2022-01-29 18:47:12 -0500

    expense of all books show on the overview page

--- client/src/Overview.js
+++ client/src/Overview.js
@@ -3,75 +3,72 @@ import BookForm from "./BookForm.js";
 import { useEffect, useState } from "react";
 
 function Overview({ user, setBooks, books, isDarkMode }) {
-  const [usersBookPile, setBookPile] = useState([]);
+    const [usersBookPile, setBookPile] = useState([]);
 
-  useEffect(() => {
-    fetch(`/api/books/user/${user.id}`)
-      .then((r) => r.json())
-      .then((bookPile) => setBookPile(bookPile));
-  }, []);
+    useEffect(() => {
+        fetch(`/api/books/user/${user.id}`)
+            .then((r) => r.json())
+            .then((bookPile) => setBookPile(bookPile));
+    }, []);
 
-  const arrayOfExpensesAll = usersBookPile.flatMap((book) =>
-    book.categories.map((category) => category.expenses.map((x) => x.cost))
-  );
+    const arrayOfExpensesAll = usersBookPile?.map((book) =>
+        book.categories.map((category) => category.expenses.map((x) => x.cost)).flat()
+    );
+    debugger
 
-  let sum = 0;
-  if (arrayOfExpensesAll?.length > 0) {
-    sum = arrayOfExpensesAll?.find(cost =>  cost > 0)
+    console.log(arrayOfExpensesAll);
 
-    let total = 0;
-    for (let i = 0; i < sum.length; i++) {
-      total = total + sum[i];
+
+    function sum(allInt) {
+        const arrayOfNumbers = allInt.flat().filter(v => v >  0)
+        const amountTotaled =  arrayOfNumbers?.reduce((p, c) => p + c, 0)
+        return  amountTotaled
     }
-  }
-  console.log(sum);
 
-  let amount = [];
+    let amount = sum(arrayOfExpensesAll)
 
-//   if (sum?.length > 0) {
-//      amount = sum?.find(cost =>  cost > 0)
-//   }
+    console.log(amount);
 
-  function placeBook(newBook) {
-    console.log("Book Packed");
-    const addBook = [...books, newBook];
-    setBooks(() => addBook);
-  }
+    function placeBook(newBook) {
+        console.log("Book Packed");
+        const addBook = [...books, newBook];
+        setBooks(() => addBook);
+    }
 
-  let totalExpenses = "Great Work Budgeting !"; /// error message ////
+    let totalExpenses = "Great Work Budgeting !"; /// error message ////
 
-  // if (books.length > 0) {
+    // if (books.length > 0) {
 
-  //   totalExpenses = books.map(book => {
-  //         return (
-  //             book.categories.map(category => { return category.expenses.map(t => { return (t.cost) }) })
-  //         )
-  //     }).reduce(reducer).split(',').map(function (item) {
-  //         return parseInt(item);
-  //     }).reduce(reducer)
-  // }
-  // else {
-  //     return ( 'hey' )
-  // }
+    //   totalExpenses = books.map(book => {
+    //         return (
+    //             book.categories.map(category => { return category.expenses.map(t => { return (t.cost) }) })
+    //         )
+    //     }).reduce(reducer).split(',').map(function (item) {
+    //         return parseInt(item);
+    //     }).reduce(reducer)
+    // }
+    // else {
+    //     return ( 'hey' )
+    // }
 
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <div className="OverView">
-        <div id="top">
-          <h1>Overview</h1>
+    return (
+        <div className={"App " + (isDarkMode ? "dark" : "light")}>
+            <div className="OverView">
+                <div id="top">
+                    <h1>Overview</h1>
 
-          <h5>Expense:{amount ? amount : totalExpenses}</h5>
+                    <h5>Expense:{amount ? amount : totalExpenses}</h5>
+                </div>
+                <div className="m">
+                    <BookShelf books={books} totalExpenses={totalExpenses} />
+                </div>
+                <div>
+                    <BookForm user={user} placeBook={placeBook} />
+                </div>
+                <footer>Bolance App | Your Best Budget ! </footer>
+            </div>
         </div>
-        <div className="m">
-          <BookShelf books={books} totalExpenses={totalExpenses} />
-        </div>
-        <div>
-          <BookForm user={user} placeBook={placeBook} />
-        </div>
-        <footer>Bolance App | Your Best Budget ! </footer>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Overview;
