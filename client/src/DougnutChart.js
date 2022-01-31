50904e577feef96b9dc0db81f624deeee9929ccc
dialloflatno <brahimdiallo23@gmail.com>
2022-01-30 20:51:42 -0500

    chart Dngh displays correct data

--- client/src/DougnutChart.js
+++ client/src/DougnutChart.js
@@ -1,21 +1,21 @@
 import {Doughnut} from 'react-chartjs-2';
 
 
-function DoughnutChart({title,bookExp}) {
+function DoughnutChart(props) {
     const data = {
+        labels: props.title,
        
         datasets: [
             {
                 label: 'Overall',
-                data: bookExp,
+                data: props.bookExp,
                 borderColor: ['rgba(255,206,86,0.2)'],
                 backgroundColor: ['rgba(232,99,132,1)',
                 'rgba(232,231,6,1)',
                 'rgba(54,192,235,1)',
-                'rgba(235,159,64,1)',
+                'rgba(25,159,64,1)',
                 'rgba(153,102,255,1)' ],
                 pointBackgroundColor: 'rgba(255,206,86,0.2)',
-                labels: title
             }
     
         ]
