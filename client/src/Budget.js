a340858de977fd4955c077b0afbfb4496a012365
dialloflatno <brahimdiallo23@gmail.com>
2022-02-04 11:19:14 -0500

    checked -- if the book value surpassed the budget that row will turn red to alert the user that there passing their budget

--- client/src/Budget.js
+++ client/src/Budget.js
@@ -1,37 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
+export default function Budget({ budget, mySum, title, category }) {
+    
+  const bookTotal = mySum(category.map((x) => x.expenses.map((xe) => xe.cost)))
+  const [alarm, setAlarm] = useState(false)
 
-export default function Budget({ budget, mySum, title ,category}) {
-
-const bookTotal = mySum(category.map((x) => x.expenses.map((xe) => xe.cost)))
-const [ alarm , setAlarm] = useState(false)
- 
-console.log(budget);
-console.log(bookTotal);
-
-
-useEffect(() => {
-    if( bookTotal > budget){
-        return setAlarm((alarm) => !alarm)
+  useEffect(() => {
+    if (bookTotal > budget) {
+      return setAlarm((alarm) => !alarm)
     }
-},[])
-console.log(alarm);
+  }, [])
 
-
-
-
-    return (
-
-        <>
-           <div className = 'budgetNotif'>
- { alarm ? <> <h5><div className='notify'>!</div>{title}:{budget} </h5></> : <h5>{title}:{budget}</h5> } 
-           </div>
-
-
-        </>
-    )
-
-
-    ///if the book value surpassed the budget that row will turn red
-    //to alert the user that there passing their budget ////////
+  return (
+    <>
+      <div className="budgetNotif">
+        {alarm ? (
+          <>
+            {' '}
+            <h5>
+              <div className="notify">!</div>
+              {title}:{budget}{' '}
+            </h5>
+          </>
+        ) : (
+          <h5>
+            {title}:{budget}
+          </h5>
+        )}
+      </div>
+    </>
+  )
 }
